49fb4642b8b9fc5bde0b096659daf01a18118b03
nakabonne <ryo@nakao.dev>
2021-02-14 11:22:00 +0900

    Add app module

---
+++ src/app.rs
@@ -0,0 +1,29 @@
+use clap::{crate_version, App, AppSettings, Arg, SubCommand};
+
+pub fn build_app() -> App<'static, 'static> {
+    let clap_color_setting = if std::env::var_os("NO_COLOR").is_none() {
+        AppSettings::ColoredHelp
+    } else {
+        AppSettings::ColorNever
+    };
+
+    let app = App::new("pbgopy")
+        .version(crate_version!())
+        .usage("pbgopy [command] [flags]")
+        .setting(clap_color_setting)
+        .setting(AppSettings::DeriveDisplayOrder)
+        .after_help(
+            "Note: `pbgopy -h` prints a short and concise overview while `pbgopy --help` gives all \
+                 details.",
+        )
+        .subcommand(SubCommand::with_name("copy")
+            .about("Copy from stdin")
+            .arg(Arg::with_name("password")
+                .help("Password to derive the symmetric-key to be used for encryption")
+                .short("p")
+                .long("password")
+            )
+        );
+
+    app
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+mod app;
+mod exit_codes;
+
+use std::env;
+use std::process;
+
+use anyhow::{anyhow, Result};
+
+use crate::exit_codes::ExitCode;
+
+fn run() -> Result<ExitCode> {
+    let matches = app::build_app().get_matches_from(env::args_os());
+
+    return Err(anyhow!(
+        "Hey",
+    ));
+}
+
 fn main() {
-    println!("Hello, world!");
+    let result = run();
+    match result {
+        Ok(exit_code) => {
+            process::exit(exit_code.into());
+        }
+        Err(err) => {
+            eprintln!("[pbgopy error]: {:#}", err);
+            process::exit(ExitCode::GeneralError.into());
+        }
+    }
 }
