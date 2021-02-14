dec700ee5aede2710e0e775f6773c15479e13822
nakabonne <ryo@nakao.dev>
2021-02-14 16:41:44 +0900

    Enable to issue PUT request

--- src/copy.rs
+++ src/copy.rs
@@ -1,20 +1,34 @@
+use std::env;
 use std::io;
 use std::io::prelude::*;
+use std::process;
 
 use anyhow::{anyhow, Result};
 
 use crate::exit_codes::ExitCode;
+
+const PBGOPY_SERVER_KEY: &str = "PBGOPY_SERVER";
 
 pub fn run() -> Result<ExitCode> {
     let mut data = String::new();
     let stdin = io::stdin();
     let mut handle = stdin.lock();
 
+    let address = match env::var(PBGOPY_SERVER_KEY) {
+        Ok(val) => val,
+        Err(err) => {
+            println!(
+                "put the pbgopy server's address into {} environment variable: {}",
+                PBGOPY_SERVER_KEY, err
+            );
+            process::exit(1);
+        }
+    };
     handle.read_to_string(&mut data)?;
-    // TODO: Run an HTTP GET method along with the data.
-    return Err(anyhow!(
-        "not implemented",
-    ));
+
+    let client = reqwest::blocking::Client::new();
+    let res = client.put(&address).body(data).send()?;
+    return Err(anyhow!("not implemented",));
 }
 
 /*
